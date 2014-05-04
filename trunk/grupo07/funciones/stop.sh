a418ab3813c2763e291fc495f86fe073eb190642
rojasagustin90@gmail.com <rojasagustin90@gmail.com@722b0ee8-6b8b-4db4-a1c3-d8d63004fff0>
2014-05-04 21:42:40 +0000

    Se completa la funcion stop.h

--- trunk/grupo07/funciones/stop.sh
+++ trunk/grupo07/funciones/stop.sh
@@ -1,5 +1,12 @@
 # Este script detiene el script pasado por parametro
 # solo si este se encuentra en ejecucion actualmente
+
+#Valido cantidad de parametros ingresados
+if [ $# -ne 1 ]; then
+	echo "Cantidad de parametros incorrecta"
+	exit 1
+fi
+
 proceso=`ps -ef | grep "$1.sh" | wc  -l`
 
 if [ ! "$proceso" -gt "1" ] 
@@ -7,5 +14,18 @@ then
 	# se supone que stop se encuentra en la misma carpeta que
 	# el script a detener (BINDIR)
 	pidof "$1.sh"
+	matar=$(kill -9 "$pidof")
+	# Verifico que se haya finalizado el proceso
+	if [[ "$matar" -eq 0 ]]; then
+		echo "Se finalizo correctamente el proceso $1"
+		exit 0
+	else
+		echo "No se pudo finalizar correctamente el proceso $1"
+		exit 2
+	fi
 	#FALTA
+#Valido que el proceso se este ejecutando.Se compara contra 1 porque el mismo grep "proceso" ya cuenta como un proceso
+elif [[ "$proceso" -eq 1 ]]; then
+	echo "El proceso $1 no se esta ejecutando"
+	exit 3
 fi
