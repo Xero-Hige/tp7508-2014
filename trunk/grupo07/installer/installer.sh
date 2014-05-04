6461cd253dbe682113f0a97443e6996f42c1c94a
Gaston.Martinez.90@gmail.com <Gaston.Martinez.90@gmail.com@722b0ee8-6b8b-4db4-a1c3-d8d63004fff0>
2014-05-04 01:07:25 +0000

    Shellcheck installer

--- trunk/grupo07/installer/installer.sh
+++ trunk/grupo07/installer/installer.sh
@@ -26,23 +26,23 @@ INSTALLERVARIABLES=( BINDIR MAEDIR NOVEDIR DATASIZE ACPTDIR INFODIR RECHDIR LOGD
 checkPerl() {
 
 	echo -e "\nCheckeando que perl 5 o superior este instalado...\n"
-	log $0 "INFO" "\nCheckeando que perl 5 o superior este instalado...\n"
+	log "$0" "INFO" "\nCheckeando que perl 5 o superior este instalado...\n"
 	MSG=$(perl -v)
 	if [ "$?" -ne "0" ]
 	then
 		echo -e "\nTP SO7508 Primer Cuatrimestre 2014. Tema C Copyright © Grupo 07\nPara instalar el TP es necesario contar con Perl 5 o superior. Efectúe su instalación e inténtelo nuevamente.\nProceso de Instalación Cancelado"
-		log $0 "ERR" "\n     TP SO7508 Primer Cuatrimestre 2014. Tema C Copyright © Grupo 07\nPara instalar el TP es necesario contar con Perl 5 o superior. Efectúe su instalación e inténtelo nuevamente.\nProceso de Instalación Cancelado"
+		log "$0" "ERR" "\n     TP SO7508 Primer Cuatrimestre 2014. Tema C Copyright © Grupo 07\nPara instalar el TP es necesario contar con Perl 5 o superior. Efectúe su instalación e inténtelo nuevamente.\nProceso de Instalación Cancelado"
 		return 1
 	else #perl existe se checkea la version
 		VERSION=$(echo "$MSG" | grep "v[0-9]*\.[0-9]*\.[0-9]*"  | sed "s/^.*v\([0-9]\)*\.[0-9]*\.[0-9]*.*$/\1/")
 		if [ "$VERSION" -ge "5" ]
 		then
 			echo -e "Perl se encuentra instalado:\n $MSG\n"
-			log $0 "INFO" "Perl se encuentra instalado\n $MSG"
+			log "$0" "INFO" "Perl se encuentra instalado\n $MSG"
 			return 0
 		else
 			echo -e "Perl se encuentra instalado pero la version es menor a la requerida\n$MSG\nCancelando instalacion..."
-			log $0 "ERR" "Perl se encuentra instalado pero la version es menor a la requerida\n$MSG\nCancelando instalacion..."
+			log "$0" "ERR" "Perl se encuentra instalado pero la version es menor a la requerida\n$MSG\nCancelando instalacion..."
 			return 1
 		fi
 	fi	
@@ -60,18 +60,18 @@ checkEnd() {
 checkTerminosYCondiciones() {
 
 	echo -e "\nTP SO7508 Primer Cuatrimestre 2014. Tema C Copyright © Grupo 07\n\n Al instalar TP SO7508 Primer Cuatrimestre 2014 UD. expresa aceptar los términos y condiciones del ACUERDO DE LICENCIA DE SOFTWARE incluido en este paquete.\nAcepta?(Y-N)"
-	log $0 "INFO" "Se muestran terminos y condiciones"
+	log "$0" "INFO" "Se muestran terminos y condiciones"
 	RESPONSE=0		
 	while [ "$RESPONSE" -eq "0" ]
 	do
 		read R
 		if [ "$R" == "y" ] || [ "$R" == "Y" ]
 		then 
-			log $0 "INFO" "El usuario acepta terminos y condiciones, se procede con las intalacion"
+			log "$0" "INFO" "El usuario acepta terminos y condiciones, se procede con las intalacion"
 			RESPONSE=1
 		elif [ "$R" == "n" ] || [ "$R" == "N" ]
 		then 
-			log $0 "INFO" "El usuario no acepta terminos y condiciones, se cancela la instalacion"
+			log "$0" "INFO" "El usuario no acepta terminos y condiciones, se cancela la instalacion"
 			RESPONSE=1
 		else
 			echo -e "\nIngrese una opcion correcta(Y-N)\n"
@@ -88,20 +88,20 @@ checkPreviouslyInstalled() {
 	if [ "$1" -eq "0" ]
 	then
 		echo -e "El programa no fue instalado, se procede con las instalacion\n"
-		log $0 "INFO" "El programa no fue instalado, se procede con las instalacion\n"
+		log "$0" "INFO" "El programa no fue instalado, se procede con las instalacion\n"
 		return 0 #se sigue con la instalacion
 	else
 		echo -e "El programa ya fue instalado\nCheckeando directorios...\n"
-		log $0 "INFO" "El programa ya fue instalado\nCheckeando directorios...\n"
+		log "$0" "INFO" "El programa ya fue instalado\nCheckeando directorios...\n"
 		checkCompleteInstallation
 		if [ "$?" -eq "0" ] #la instalacion estaba completa
 		then
 			MESSAGE+="\nProceso de instalacion cancelado\n"			
-			log $0 "INFO" "$MESSAGE"
+			log "$0" "INFO" "$MESSAGE"
 			echo -e "$MESSAGE"
 			return 1 #se cancela la instalacion
 		else # la instalacion no estaba completa
-			log $0 "WAR" "$MESSAGE"
+			log "$0" "WAR" "$MESSAGE"
 			echo -e "$MESSAGE"
 			RESPONSE=0			
 			while [ "$RESPONSE" -eq "0" ]
@@ -110,12 +110,12 @@ checkPreviouslyInstalled() {
 				read R
 				if [ "$R" == "y" ] || [ "$R" == "Y" ]
 				then 
-					log $0 "INFO" "El usuario elige continuar con la instalacion"
+					log "$0" "INFO" "El usuario elige continuar con la instalacion"
 					RESPONSE=1 
 					return 2	
 				elif [ "$R" == "n" ] || [ "$R" == "N" ]
 				then 
-					log $0 "INFO" "El usuario elige cancelar la instalacion"
+					log "$0" "INFO" "El usuario elige cancelar la instalacion"
 					RESPONSE=2
 					return 1
 				else
@@ -175,11 +175,11 @@ endInstallation() {
 			read NP
 			if [ "$NP" == "y" ] || [ "$NP" == "Y" ]
 			then 
-				log $0 "INFO" "El usuario acepta, se completa la instalacion"
+				log "$0" "INFO" "El usuario acepta, se completa la instalacion"
 				RESPONSE=1
 			elif [ "$NP" == "n" ] || [ "$NP" == "N" ]
 			then 
-				log $0 "INFO" "El usuario no acepta, se piden de nuevo los valores"
+				log "$0" "INFO" "El usuario no acepta, se piden de nuevo los valores"
 				RESPONSE=2
 			else
 				echo -e "\nIngrese una opcion correcta(Y-N)\n"
@@ -206,13 +206,13 @@ finish() {
 		read NP
 		if [ "$NP" == "y" ] || [ "$NP" == "Y" ]
 		then 
-			log $0 "INFO" "El usuario acepta, se completa la instalacion"
+			log "$0" "INFO" "El usuario acepta, se completa la instalacion"
 			createDirs
 			updateConfFile
 			exit 0
 		elif [ "$NP" == "n" ] || [ "$NP" == "N" ]
 		then 
-			log $0 "INFO" "El usuario no acepta, se cancela la instalacion"
+			log "$0" "INFO" "El usuario no acepta, se cancela la instalacion"
 			exit 1
 		else
 			echo -e "\nIngrese una opcion correcta(Y-N)\n"
