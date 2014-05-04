0c01dd24ac3e1fb27d22392502abf1eccbb0ff2d
rojasagustin90@gmail.com <rojasagustin90@gmail.com@722b0ee8-6b8b-4db4-a1c3-d8d63004fff0>
2014-05-04 22:11:24 +0000

    Cambios en start y stop

--- grupo07/funciones/start.sh
+++ grupo07/funciones/start.sh
@@ -1,10 +1,31 @@
 # Este script ejecuta el script pasado por parametro
 # solo si este no se encuentra en ejecucion actualmente
-proceso=`ps -ef | grep "$1.sh" | wc  -l`
 
-if [ ! "$proceso" -gt "1" ] 
-then
+
+#Valido cantidad de parametros ingresados
+if [ $# -ne "1" ]; then
+	echo "Cantidad de parametros incorrecta"
+	exit 1
+fi
+#Verifico que este inicializado el ambiente
+ENVIRONMENT=1 #solo para probar
+if [ "$ENVIRONMENT" -ne "1" ]; then
+	echo "El ambiente no está inicializado"
+	exit 2
+fi
+#proceso=`ps -ef | grep "$1.sh" | wc  -l`
+proceso=`ps -ef | grep "$1" | wc  -l`
+echo $proceso
+if [ "$proceso" -gt "1" ] 
+	then
+	#El proceso se esta ejecutando
+	echo "El proceso $1 ya se esta ejecutando"
+	exit 3
+else
+	#Sino lo ejecuto
 	# se supone que start se encuentra en la misma carpeta que
 	# el script a ejecutar (BINDIR)
-	`./"$1.sh"`
+	#`./"$1.sh"`
+	$(./"$1")
+	exit 0
 fi
