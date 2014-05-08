7b36186302f4a55017607c33e848a7e7af7feb7c
bruno290@gmail.com <chulmi@users.noreply.github.com>
2014-05-08 17:55:09 +0000

    Emprolijado el código y terminada la documentación de reporting.pl

--- Integracion/grupo07/bin/reporting.pl
+++ Integracion/grupo07/bin/reporting.pl
@@ -4,9 +4,7 @@ use Term::ReadKey;
 use File::Spec::Functions qw(catfile);
 
 use Env qw(ENVIRONMENT INFODIR MAEDIR INFON);
-#$MAEDIR = "./"; # Hacerlo posta
 $MAEFILE = "super.mae"; 
-#$INFODIR = "./"; #Hacerlo posta
 $PRESDIR = "pres";
 $REF_BOUNDARY = 100;
 $OBS_LE = "(*)";
@@ -15,9 +13,10 @@ $OBS_NF = "(***)";
 $INFO_LINE = "$OBS_LE Precio menor o igual al Precio Cuidado.\n$OBS_GT Precio mayor al Precio Cuidado.\n$OBS_NF Precio Cuidado no encontrado.\n";
 $NO_RESULT_MSG = "No hay resultados para la consulta\n";
 
+# Usando los datos de super.mae, guarda los datos de los supermercados en el hash recibido por parámetro
 sub makeSupermarketsHash { 
     my($supermarkets_ref) = @_[0];
-    open(SUPER_MAE_H, "<".catfile($MAEDIR, $MAEFILE)) || die "No se pudo abrir el super.mae\n";
+    open(SUPER_MAE_H, "<".catfile($MAEDIR, $MAEFILE)) || die "No se pudo abrir super.mae\n";
     while (<SUPER_MAE_H>) {
         my($id, $province, $super_name) = ($_ =~ /^(\d+);(.+);(.*)(?:;.*){3}$/);
         ${$supermarkets_ref}{$id} = $province."-".$super_name;
@@ -25,6 +24,7 @@ sub makeSupermarketsHash {
     close(SUPER_MAE_H);
 }
 
+# Lee una tecla del stdin y la devuelve
 sub readKey {
     ReadMode('cbreak');
     my($key) = ReadKey(0);
@@ -32,6 +32,7 @@ sub readKey {
     return $key;
 }
 
+# Imprime a stdout el mensaje de ayuda del comando
 sub printHelp {
     print "\nAyuda\n\n";
     print "El programa reporting analiza las listas presupuestadas y realiza informes sobre los resultados\n".
@@ -49,13 +50,12 @@ sub printHelp {
                 "consecutivo al último informe realizado.\n\n\t-x: Filtra por provincia-supermercado. Despliega un ".
                 "menú con todas las opciones disponibles, y permite elegir todos, algunos o ninguno para que sea ".
                 "considerado en el reporte.\n\n\t-u: Filtra por usuario. Muestra una lista de usuarios disponibles y ".
-                "permite elegir las listas de quienes serán analizadas para realizar el informe.\n\n\t-s: Sale del ".
-                "programa.\n\n";    
+                "permite elegir las listas de quienes serán analizadas para realizar el informe.\n\n";    
     print "Presione una tecla para continuar...\n\n";
     readKey;
 }
 
-#Esto se reemplaza por INFON, pero hay que setear la variable antes
+# Obtiene el número de secuencia para nombrar los archivos de informes
 sub getNextDescriptorNumber {
     my ($ret_n) = 0;
     opendir(INFODIR_H, "$INFODIR");
@@ -69,6 +69,8 @@ sub getNextDescriptorNumber {
     return (sprintf("%03d", ++$ret_n));
 }
 
+# Recibe una lista con las opciones ingresadas como parámetro y una referencia al hash donde se
+# guardarán las opciones procesadas y validadas
 sub processOptions {
     my(@options_input) = @{@_[0]};
     my($href) = @_[1];
@@ -101,10 +103,10 @@ sub processOptions {
         } elsif ($_ eq "a") {
             ${$href}{"a"} = 1;
             last;
-        } elsif ($_ eq "s") {
-            ${$href}{"s"} = 1;
         }
     }
+    # Removes, if present, the -x option in case it isn used (like for options -f, or -x
+    # without -d or -m)
     if ((exists ${$href}{"r"}) && ! (exists ${$href}{"m"} || exists ${$href}{"d"})) {
         delete(${$href}{"x"});
     } elsif (exists ${$href}{"f"}) {
@@ -112,6 +114,8 @@ sub processOptions {
     }
 }
 
+# Recibe una referencia al hash de opciones procesadas, y devuelve 1 si alguna opción de consulta 
+# (r, m, d y/o f) fue elegida o 0 si ninguna se encuentra en el hash
 sub checkProcessingOptions {
     foreach (keys(%{@_[0]})) {
         return 1 if ($_ =~ /[r|m|d|f]/);
@@ -119,6 +123,7 @@ sub checkProcessingOptions {
     return 0;
 }
 
+# Imprime una lista de provincia-supermercado y su respectivo id, a partir del hash recibido
 sub printAvailableSupermarkets {
     my($supermarkets_ref) = @_[0];
     print "\nLos supermercados disponibles son: \n";
@@ -128,6 +133,7 @@ sub printAvailableSupermarkets {
     print "\n";
 }
 
+# Lee por stdin una lista de id de supermercados elegidos y devuelve una lista con los mismos
 sub selectFilters {
     print "Seleccione los supermercados que desea incluir ingresando los números correspondientes ".
                  "a cada supermercado separados por comas (,). Ingrese 'todos' si desea elegirlos todos. Ingrese ".
@@ -136,6 +142,9 @@ sub selectFilters {
     return (<STDIN> =~ /,?\s*(\w*)\s*,?/g);
 }
 
+# Recibe una referencia al hash de supermercados, una al hash donde se guardaran los supermercados
+# elegidos, y una lista de elecciones. Procesa la lista y agrega los supermercados que correspondan
+# al hash
 sub processSelectedFilters {
     my($supermarkets_ref) = @_[0];
     my($selected_ref) = @_[1];
@@ -158,12 +167,16 @@ sub processSelectedFilters {
     print "\n";
 }
 
+# Devuelve 1 o 0, dependiendo si se confirmo o rechazo la selección de filtros
 sub confirmFilterSelection {
     print "Es correcto? Presione s para continuar, n para elegir de vuelta.\n\n";
     return 1 if (readKey =~ /[s|S]/);
     return 0;
 }
 
+# Recibe una referencia al hash de opciones, una al hash de supermercados, y una al hash
+# donde se guardaran los supermercados elegidos. Al finalizar su ejecución, dicho hash 
+# tiene cargado los filtros de supermercados elegidos
 sub makeFilterHash {
     my(%options) = %{@_[0]};
     my($supermarkets_ref) = @_[1];
@@ -187,6 +200,7 @@ sub makeFilterHash {
     }
 }
 
+# Obtiene y devuelve una lista de los usuarios, a partir de las listas de compras presupuestadas
 sub getUserList {
     my(%users) = ();
     opendir(PRES_H, catfile($INFODIR, $PRESDIR));
@@ -199,6 +213,8 @@ sub getUserList {
     return (keys %users);
 }
 
+# Recibe la lista de usuarios. Imprime la misma, y toma por stdin una lista de usuarios elegidos
+# para procesar sus consultas
 sub selectUsers {
     my(@users_list) = @_;
     print "Usuarios con listas presupuestadas: \n";
@@ -210,6 +226,9 @@ sub selectUsers {
     return (<STDIN> =~ /,?\s*(\d*)\s*,?/g);
 }
 
+# Recibe una referencia al hash que se usará como filtro de usuarios, una referencia a la lista
+# de usuarios, y una lista de usuarios elegidos. Procesa la lista de elegidos, y agrega al hash
+# los que correspondan
 sub processSelectedUsers {
     my($users_filter_ref) = @_[0];
     my(@users_list) = @{@_[1]};
@@ -222,12 +241,19 @@ sub processSelectedUsers {
     }
 }
 
+# Recibe una referencia al hash que se usará como filtro de usuarios. Ofrece una lista de usuarios
+# disponibles y permite elegir los que integraran el filtro, procesa la selección, y agrega los que
+# correspondan al hash
 sub makeUserFilter {
     my($users_filter_ref) = @_[0];
     my(@users_list) = getUserList;
     processSelectedUsers($users_filter_ref, \@users_list, selectUsers(@users_list));
 }
 
+# Recibe la ruta al archivo de presupuesto a procesar, una referencia al hash de opciones, una 
+# referencia al filtro de supermercados, una al hash donde guardar los resultados de referencia 
+# (Precios Cuidados), y una al hash donde guardar los resultados de productos. Procesa el archivo y
+# llena los hash con los resultados que correspondan a las opciones elegidas.
 sub processList {
     my($usr_file) = @_[0];
     my(%options) = %{@_[1]};
@@ -235,10 +261,9 @@ sub processList {
     my($references_result_ref) = @_[3];
     my($items_result_ref) = @_[4];
     my(%references_result, %items_result);
-    open(LIST, "<$usr_file") || die "No se pudo abrir el archivo de lista\n";
+    open(LIST, "<$usr_file") || die "No se pudo abrir el archivo de lista presupuestada\n";
     while (<LIST>) {
         my($item_id, $item_name, $super_id, $item_info, $price) = split(/;/, $_ );
-        #print $item_id." - ".$item_name." - ".$super_id." - ".$item_info." - ".$price."\n";
         chomp($price);
         # Checks if data is missing, and stores it if required (-f) or next if it doens't
         if ($super_id eq "") {
@@ -265,10 +290,13 @@ sub processList {
     %{$items_result_ref} = %items_result;
 }
 
+# Función de comparación para sort que ordena según el item id
 sub sortByItemId {
     return (@_[0] <=> @_[1]);
 }
 
+# Función de comparación para sort que ordena según el id de supermercado, y por id de item como 
+# segunda condición
 sub sortBySuperId {
     my($a, $b, $result_ref) = @_;
     my(@data_a) = @{${$result_ref}{$a}};
@@ -276,6 +304,10 @@ sub sortBySuperId {
     return (($data_a[3] <=> $data_b[3]) || ($a <=> $b));
 }
 
+# Recibe una referencia al hash con el resultado, una referencia a la lista de supermercados, y una 
+# a la función de comparación a utilizar para ordenar. Devuelve una lista ordenada con la 
+# información del resultado como debe mostrarse en el caso de opción de consulta simple (-r, -m, -d 
+# o -f)
 sub makeOrderedSimpleResultList{
     my(%result) = %{@_[0]};
     my($supermarkets_ref) = @_[1];
@@ -289,6 +321,10 @@ sub makeOrderedSimpleResultList{
     return @ordered_list;
 }
 
+# Recibe una referencia al hash con el resultado de precios de referencia, una al hash de resultado
+# de productos, una a la lista de supermercados, y una a la función de comparación a utilizar para 
+# ordenar. Devuelve una lista ordenada con la información del resultado como debe mostrarse en el 
+# caso de opción de consulta doble (-rm o -rd)
 sub makeOrderedDoubleResultList {
     my(%references) = %{@_[0]};
     my(%items) = %{@_[1]};
@@ -313,6 +349,9 @@ sub makeOrderedDoubleResultList {
     return @ordered_list;
 }
 
+# Recibe una referencia al hash de opciones, una referencia a la lista de filtro de supermercados, 
+# una al hash de resultados de precios de referencia y una al hash de resultado de productos. Genera
+# la lista con el resultado para mostrar y lo devuelve
 sub makeReport {
     my(%options) = %{@_[0]};
     my($filter_ref) = @_[1];
@@ -335,7 +374,11 @@ sub makeReport {
         return makeOrderedSimpleResultList($sort_hash_ref, $filter_ref, $sort_sub_ref);
     }
 }
-    
+
+# Recibe una referencia al hash de opciones, una al filtro de supermercados, una al de usuarios y, 
+# si se necesita (por la opción -w) un handler de un archivo donde grabar. Imprime a stdout, y al 
+# archivo si corresponde, el header del resultado (Consulta, contenido de filtros, y otra 
+# información necesaria)
 sub printHeader {
     my(%options) = %{@_[0]};
     my($supermarkets_ref) = @_[1];
@@ -358,6 +401,9 @@ sub printHeader {
     }
 }
     
+# Recibe una referencia al hash de opciones, una al filtro de supermercados, una al de usuarios y, 
+# si se necesita (por la opción -w) un handler de un archivo donde grabar. Imprime a stdout, y al 
+# archivo si corresponde, el resultado de la consulta realizada
 sub printResults {
     my(%options) = %{@_[0]};
     my($usr_file) = @_[1];
@@ -371,78 +417,62 @@ sub printResults {
     }
 }
 
-sub printMenu {
-    print "\nReporting - Herramienta de informes de RETAILC\n\n";
-    print "Opciones:\n";
-    print "\t-a: Imprime la ayuda del programa\n";
-    print "\t-r: Reporte de precios de referencia. Combinable con -m y -d\n";
-    print "\t-m: Reporte de menor precio. Combinable con -r\n";
-    print "\t-d: Reporte de donde comprar. Combinable con -r\n";
-    print "\t-f: Reporte de faltantes\n";
-    print "\t-x: Filtrar por provincia-supermercado\n";
-    print "\t-u: Filtrar por usuario\n";
-    print "\t-s: Salir del programa\n";
-    print "\n";
-}
+############################################### Main ###############################################
 
+# Checks for system initialization
 die "No se realizó la inicialización de ambiente\n" if ($ENVIRONMENT != 1);
+# Checks if the process is already running
 ($process_name) = ($0 =~ /\/(.+)$/);
 die "Reporting ya está ejecutándose\n" if (`ps -C  $process_name -o pid=`);
-#$keep_running = 1;
-#while ($keep_running) {
-    %supermarkets = ();
-    makeSupermarketsHash(\%supermarkets);
-    #printMenu;
-    #print "\tIngresar las opciones deseadas: ";
-    $parameters = "";
-    $parameters = $parameters.$_ foreach(@ARGV);
-    #@options_input = (@ARGV =~ /-(\w)(\w*)/g);
-    @options_input = ($parameters =~ /-(\w)(\w*)/g);
-    %options = (); # Cleans the options hash
-    processOptions(\@options_input, \%options);
-    if (exists $options{"s"}) {
-        #$keep_running = 0;
-        #next;
-    }
-    if (exists $options{"a"}) {
-        printHelp;
-        #next;
-        die;
-    }
-    if (! checkProcessingOptions(\%options)) {
-        print "\nAlguna de las siguientes opciones debe estar presentes: -r -m -rm -d -rd -f\n\n";
-        #next;
-        die;
-    }
-    # Users filter
-    %users_filter = ();
-    if (exists $options{"u"}) {
-        makeUserFilter(\%users_filter);
-    }
-    %supermarkets_filter = (); # Cleans the filter hash
-    makeFilterHash(\%options, \%supermarkets, \%supermarkets_filter);
-    if (exists $options{"w"}) {
-        $ext = getNextDescriptorNumber;
-        open(INFO_H, ">".catfile($INFODIR,"info_$ext"));
-    }
-    printHeader(\%options, \%supermarkets_filter, \%users_filter, INFO_H);
-    opendir(PRES_H, catfile($INFODIR, $PRESDIR));
-    foreach $file (readdir(PRES_H)) {
-        # Skips . .. and any other file that doesn't have the required format
-        next unless ($file =~ /^.*\..{3}$/);
-        # Filter by user
-        ($user_name) = ($file =~ /^(.*)\.\w{3}$/);
-        next unless ((scalar keys %users_filter < 1) || (exists $users_filter{$user_name}));
-        %references_result = ();
-        %items_result = ();
-        $usr_file = catfile($INFODIR, $PRESDIR, $file);
-        processList($usr_file, \%options, \%supermarkets_filter, \%references_result, \%items_result);
-        @result_list = makeReport(\%options, \%supermarkets_filter, \%references_result, \%items_result);
-        push(@result_list, $NO_RESULT_MSG) if ($#result_list < 0);
-        printResults(\%options, $usr_file, \@result_list, INFO_H);
-    }
-    print "\nPresione una tecla para continuar...\n\n";
-    readKey;
-    closedir(PRES_H);
-    close(INFO_H) if (exists $options{"w"});
-#}
+# Supermarkets hash initialization
+%supermarkets = ();
+makeSupermarketsHash(\%supermarkets);
+# Parse and process the options arguments
+$parameters = "";
+$parameters = $parameters.$_ foreach(@ARGV);
+@options_input = ($parameters =~ /-(\w)(\w*)/g);
+%options = (); # Cleans the options hash
+processOptions(\@options_input, \%options);
+# Help message
+if (exists $options{"a"}) {
+    printHelp;
+    die;
+}
+# Checks for query options
+if (! checkProcessingOptions(\%options)) {
+    print "\nAlguna de las siguientes opciones debe estar presentes: -r -m -rm -d -rd -f\n\n";
+    die;
+}
+# Users filter
+%users_filter = ();
+if (exists $options{"u"}) {
+    makeUserFilter(\%users_filter);
+}
+# Supermarket filter
+%supermarkets_filter = ();
+makeFilterHash(\%options, \%supermarkets, \%supermarkets_filter);
+if (exists $options{"w"}) {
+    $ext = getNextDescriptorNumber;
+    open(INFO_H, ">".catfile($INFODIR,"info_$ext"));
+}
+# Result processing and displaying (and writing, if required)
+printHeader(\%options, \%supermarkets_filter, \%users_filter, INFO_H);
+opendir(PRES_H, catfile($INFODIR, $PRESDIR));
+foreach $file (readdir(PRES_H)) {
+    # Skips . .. and any other file that doesn't have the required format
+    next unless ($file =~ /^.*\..{3}$/);
+    # Filter by user
+    ($user_name) = ($file =~ /^(.*)\.\w{3}$/);
+    next unless ((scalar keys %users_filter < 1) || (exists $users_filter{$user_name}));
+    %references_result = ();
+    %items_result = ();
+    $usr_file = catfile($INFODIR, $PRESDIR, $file);
+    processList($usr_file, \%options, \%supermarkets_filter, \%references_result, \%items_result);
+    @result_list = makeReport(\%options, \%supermarkets_filter, \%references_result, \%items_result);
+    push(@result_list, $NO_RESULT_MSG) if ($#result_list < 0);
+    printResults(\%options, $usr_file, \@result_list, INFO_H);
+}
+print "\nPresione una tecla para continuar...\n\n";
+readKey;
+closedir(PRES_H);
+close(INFO_H) if (exists $options{"w"});
