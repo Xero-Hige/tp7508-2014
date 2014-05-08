57b3d84087be650439e3d671b848ff257a305f9e
Gaston Martinez <Xero-Hige@users.noreply.github.com>
2014-05-08 04:28:23 +0000

    Licencia

--- grupo07 (listo para instalar)/exe/listener.sh
+++ grupo07 (listo para instalar)/exe/listener.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
 
-#############----------------------------------------------------------------------------------
-#NOVEDIR="./novedir"
-#MAEDIR="."
-#ACEPDIR="./acept"
-#RECHDIR="./rech"
-#############----------------------------------------------------------------------------------
+#*****************************************************************************#
+#listener.sh								      
+
+#Copyright 2014: Agustin Rojas			<mail>
+#		 Bruno Merlo Schurmann		<mail>
+#		 Fabrizio Graffe		<mail>
+#		 Gaston Alberto Martinez	<gaston.martinez.90@gmail.com>
+#		 Leandro Gallipi		<mail>
+#		 Luciano Raineri Marchina	<mail>
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see <http://www.gnu.org/licenses>
+#*****************************************************************************#
 
 sed_escape_filter="s|\([\/\*\?\ ]\)|\\1|g"
 
